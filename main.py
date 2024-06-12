ad0b631df47b961e4e0a4ff30a4e583b0adc2f02
Min Thura Zaw <minthurazaw619@gmail.com>
2024-06-12 15:43:51 +0700

    update app

--- main.py
+++ main.py
@@ -11,4 +11,18 @@ templates = Jinja2Templates(directory='templates')
 @app.get("/", response_class=HTMLResponse)
 async def read_root(request: Request):
     current_datetime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    return templates.TemplateResponse("index.html", {"request": request, "current_datetime": current_datetime})
+    return templates.TemplateResponse("index.html",
+                                      {"request": request, "current_datetime": current_datetime})
+
+
+@app.get("/time-difference/", response_class=HTMLResponse)
+async def calculate_time_difference(request: Request, time_str: str):
+    try:
+        user_time = datetime.strptime(time_str, "%Y-%m-%d %H:%M:%S")
+        current_time = datetime.now()
+        time_difference = current_time - user_time
+        return templates.TemplateResponse("time_difference.html",
+                                          {"request": request, "time_difference": time_difference})
+    except ValueError:
+        return templates.TemplateResponse("time_difference.html",
+                                          {"request": request, "error_message": "Invalid time format. Please provide time in the format 'YYYY-MM-DD HH:MM:SS'."})
