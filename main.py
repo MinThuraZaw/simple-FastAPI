64e876a50d748eabeb3e49bc74b4f10181b296b0
Min Thura Zaw <minthurazaw619@gmail.com>
2024-06-25 19:53:01 +0700

    update app with login

--- main.py
+++ main.py
@@ -1,28 +1,30 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Form
 from fastapi.templating import Jinja2Templates
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi.staticfiles import StaticFiles
 from datetime import datetime
 
-
 app = FastAPI()
-templates = Jinja2Templates(directory='templates')
+templates = Jinja2Templates(directory="templates")
+
+# Mount the static files directory
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+# @app.get("/", response_class=HTMLResponse)
+# async def read_root(request: Request):
+#     current_datetime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+#     return templates.TemplateResponse("index.html", {"request": request, "current_datetime": current_datetime})
 
 
 @app.get("/", response_class=HTMLResponse)
-async def read_root(request: Request):
-    current_datetime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    return templates.TemplateResponse("index.html",
-                                      {"request": request, "current_datetime": current_datetime})
+async def login_form(request: Request):
+    return templates.TemplateResponse("login.html", {"request": request})
 
 
-@app.get("/time-difference/", response_class=HTMLResponse)
-async def calculate_time_difference(request: Request, time_str: str):
-    try:
-        user_time = datetime.strptime(time_str, "%Y-%m-%d %H:%M:%S")
-        current_time = datetime.now()
-        time_difference = current_time - user_time
-        return templates.TemplateResponse("time_difference.html",
-                                          {"request": request, "time_difference": time_difference})
-    except ValueError:
-        return templates.TemplateResponse("time_difference.html",
-                                          {"request": request, "error_message": "Invalid time format. Please provide time in the format 'YYYY-MM-DD HH:MM:SS'."})
+@app.post("/login", response_class=HTMLResponse)
+async def login(request: Request, username: str = Form(...), password: str = Form(...)):
+    if username == "user" and password == "password":
+        return templates.TemplateResponse("welcome.html", {"request": request, "username": username})
+    else:
+        return templates.TemplateResponse("login.html", {"request": request, "error": "Invalid credentials"})
